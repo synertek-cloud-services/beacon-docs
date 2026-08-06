abce35036057c651dbb93e5cce1492665863c5b0
Jeremy Schaffer <jeremys@codenexus.org>
2026-08-06 00:24:30 -0700

    docs: use runbeacon documentation domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://beacon.synertekcs.com',
+  site: 'https://docs.runbeacon.net',
   output: 'static',
   integrations: [
     mdx(),
